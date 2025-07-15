3c004bc86c589e3e39f6a214bcef44562aed0b5d
Nekomi Fujiwara <morvenhui@gmail.com>
2025-07-15 19:16:35 +0800

    More colors

--- lua/FujiIdol/init.lua
+++ lua/FujiIdol/init.lua
@@ -10,25 +10,35 @@ function M.setup()
 	vim.cmd 'highlight clear'
 	vim.cmd 'syntax reset'
 
-	vim.o.background = 'dark';
 	vim.g.colors_name = 'NekomiNight';
 	vim.opt.cursorline = true;
 
 	--highlight groups
 	local highlights = {
+		--General Highlights
 		Normal = {fg = colors.nekomi7, bg = colors.nekomi1},
 		Visual = {bg = colors.nekomi2},
 		Comments = {fg = colors.nekomi5, italic = true},
 		Keyword = {fg = colors.magenta, bold = true},
 		String = {fg = colors.green},
 		Number = {fg = colors.orange},
-		Error = {fg = colors.red},
 		Function = {fg = colors.blue},
 		Identifier = {fg = colors.cyan1},
 		LineNr = {fg = colors.nekomi3},
-		CursorLineNr = {fg = colors.magenta1},
-		CursorLine = {bg = colors.nekomi0},
 		Cursor = {fg = colors.nekomi1, bg = colors.magenta},
+		CursorLineNr = {fg = colors.magenta1, bg = colors.nekomi0},
+		CursorLine = {bg = colors.nekomi0},
+		StatusLine = {fg = colors.blue, bg = colors.nekomi0},
+		MsgArea = {bg = colors.nekomi0, fg = colors.magenta1},
+		--diagnostic
+		DiagnosticWarn = {fg = colors.yellow1},
+		DiagnosticError = {fg = colors.red1},
+		DiagnosticInfo = {fg = colors.green1},
+		DiagnosticHint = {fg = colors.blue1},
+		DiagnosticUnderLineWarn = {sp = colors.yellow1},
+		DiagnosticUnderLineError = {sp = colors.red1},
+		DiagnosticUnderLineInfo = {sp = colors.green1},
+		DiagnosticUnderLineHint = {sp = colors.blue1},
 	}
 
 	--apply highlights 
